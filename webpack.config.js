8c9c6975443c89bbae791dd27cce66c77415e982
arihito <arihito.m@gmail.com>
2017-03-15 18:54:22 +0900

    first build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports =  {
+  entry: __dirname + '/src/test.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query:
+          {
+            presets: ['es2015','stage-0']
+          }
+        }
+      ]
+  }
+};
